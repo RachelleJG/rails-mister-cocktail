bddec7bf90b27a05728756e3a137292a9cc1ef23
Rachelle Jones-Gatineau <rachelle.mae.jones@gmail.com>
2019-02-15 08:33:08 -0500

    playing with bootstrap buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :new, :create, :show, :destroy]  do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
 end
+  # Cocktail controller
+  #get 'cocktails', to: 'cocktails#index'
+  #post 'cocktails', to: 'cocktails#create'
+  #get 'cocktails/new', to: 'cocktails#new'
+  #get 'cocktails/:id', to: 'cocktails#show'
+
+  # Doses controller
+  #get 'doses/new', to: 'doses#new'
+  #get 'doses', to: 'doses#create'
+  #delete 'doses/:id', to: 'doses#destroy'
